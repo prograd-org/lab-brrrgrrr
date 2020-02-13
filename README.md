29ad799aff05cbaf41c55d1b411a6d23f1be2c6a
Teacher-Venkatesh <12venkat21@gmail.com>
2020-02-13 22:59:43 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,43 @@
+<h1>ProGrad</h1>
+
+## LAB | BRRRGRRR
+
+Mama Jenny is the best cook you've ever met. She makes the most amazing burgers in her tiny diner across the street. Mama is extremely popular and has recently had the idea to take her business online. Unfortunately, she isn't the best at web development. I've heard you are though. Help Mama create a website that'll attract more audience and help her make a living through it. Go to the `src/app.js` file and complete all the unfinished code to satiate her sweet tooth.
+
+## Requirements
+
+- Fork this repo
+- Clone this repo
+- Practice DOM features
+
+## Submission
+
+- Upon completion, run the following commands:
+
+  ```
+  git add .
+  git commit -m "done"
+  git push origin master
+  ```
+
+- Create Pull Request so your TAs can check up your work.
+
+## Trial 1: What do we have here?
+
+For your first task, you've got to enable the different layers of the burger on the screen. Create a `render()` for each ingredient and make sure that it displays the selected player.
+
+## Trial 2: Give me an extra cheese!
+
+Setup `EventListeners`, so that we can add or remove different ingredients by clicking on the respective ingredient buttons. That's the whole point of it.
+
+## Challenge 1: Do I need more cheese?
+
+Make the ingredient buttons popup or not based on their current active state. This would help customers identify what they've selected and what not.
+
+## Challenge 2: Note my order!
+
+Show the customer only the ingredients that they've selected on the chalk board.
+
+## Judgement 1: Bill the BRRRGRR!!
+
+The customer has had a good time customizing his brrgrr. Time for him to pay up. Use ingredient prices to calculate how much a burger costs and display it to the customer.
---
+++ src/app.js
@@ -0,0 +1,89 @@
+// Write your code on this file. Please don't change the existing code
+// unless absolutely needed.
+
+//Initial price of the burger
+var wholeWheatBun = 10;
+
+//Ingredients of the burger along with the price
+// Clue: the name is same as the textcontent of the button. Will be useful later on :)
+var ingredients = {
+  Patty: 80,
+  Cheese: 10,
+  Tomatoes: 20,
+  Onions: 20,
+  Lettuce: 20
+};
+
+//Current state of the ingredients in the burger
+var state = {
+  Patty: true,
+  Cheese: true,
+  Tomatoes: true,
+  Onions: true,
+  Lettuce: true
+};
+
+// This function renders the entire screen everytime the state changes accordingly
+function renderAll() {
+  renderPatty();
+  renderCheese();
+  renderTomatoes();
+  renderOnions();
+  renderLettuce();
+  renderButtons();
+  renderIngredientsBoard();
+  renderPrice();
+}
+
+function renderPatty() {
+  let $patty = document.querySelector("#patty");
+  //you can also use getElementById
+  if (state.Patty) {
+    $patty.style.display = "inherit";
+  } else {
+    $patty.style.display = "none";
+  }
+}
+
+function renderCheese() {
+  //Trial 1 - Change the visibility of cheese based on state by manipulating the DOM
+}
+
+function renderTomatoes() {
+  //Trial 1 - Change the visibility of Tomatoes based on state by manipulating the DOM
+}
+
+function renderOnions() {
+  //Trial 1 - Change the visibility of Onions based on state by manipulating the DOM
+}
+
+function renderLettuce() {
+  //Trial 1 - Change the visibility of Lettuce based on state by manipulating the DOM
+}
+
+document.querySelector(".btn-patty").onclick = function () {
+  state.Patty = !state.Patty;
+  renderAll();
+};
+
+// Trial 2 - Setup event listener for the cheese button
+
+
+// Trial 2 - Setup event listener for the tomatoes button
+
+
+// Trial 2 - Setup event listener for the onion button
+
+
+// Trial 2 - Setup event listener for the lettuce button
+
+
+//Challenge 1 - Add/Remove the class active to the buttons based on state
+
+
+//Challenge 2 - Render only the items selected in the ingredients board based on the state
+
+
+//Judgement 1
+//In the p element having price-details as the class, display the calculated
+//price based on ingredients
